a5804d5955fe44e4166040657d357c7227c6b236
Shagun Sodhani <sodhani@fb.com>
2021-09-18 05:00:45 -0700

    Return full name of the experiment in slurm

--- xplogger/experiment_manager/slurm/slurm.py
+++ xplogger/experiment_manager/slurm/slurm.py
@@ -15,12 +15,12 @@ def get_info_from_slurm(job_id: str) -> dict[str, Any]:
 
 
 def get_running_jobs() -> pd.DataFrame:
-    command = "squeue -u $USER -o '%.8Q %.10i %.3P %.9j %.6u %.2t %.16S %.10M %.10l %.5D %.12b %.2c %.4m %R' -S -t,-p,i"
+    command = "squeue -u $USER -o '%.8Q %.15i %.3P %.9j %.6u %.2t %.16S %.10M %.10l %.5D %.12b %.2c %.4m %R' -S -t,-p,i"
     result = subprocess.check_output(command, shell=True).decode("utf-8").strip()
     result_list = [x.strip() for x in result.split("\n") if x.strip()]
     keys = result_list[0].split()
     info = [
         {key: value for (key, value) in zip(keys, values.split())}
-        for values in result[1:]
+        for values in result_list[1:]
     ]
     return pd.DataFrame.from_dict(info)
