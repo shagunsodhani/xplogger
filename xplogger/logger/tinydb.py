f9cb930947e1220b305df3c7f64952c32e0eeb41
Shagun Sodhani <sodhani@fb.com>
2022-01-28 14:19:49 -0800

    Add tinydb logger

---
+++ xplogger/logger/tinydb.py
@@ -0,0 +1,42 @@
+"""Functions to interface with TinyDB."""
+
+
+from tinydb import TinyDB
+from tinyrecord import transaction
+
+from xplogger.logger.base import Logger as BaseLogger
+from xplogger.types import ConfigType, LogType
+
+
+class Logger(BaseLogger):
+    """Logger class that writes to TinyDB."""
+
+    def __init__(self, config: ConfigType):
+        """Initialise the TinyDB Logger.
+
+        Args:
+            config (ConfigType): config to initialise the TinyDB logger.
+                It must have one key: path.
+        """
+        super().__init__(config=config)
+        keys_to_check = [
+            "path",
+        ]
+        if not all(key in config for key in keys_to_check):
+            key_string = ", ".join(keys_to_check)
+            raise KeyError(
+                f"One or more of the following keys missing in the config: {key_string}"
+            )
+        self.path = config["path"]
+        self.logger_types = {"config", "message", "metadata"}
+
+    def write(self, log: LogType) -> None:
+        """Write the log to TinyDB.
+
+        Args:
+            log (LogType): Log to write
+        """
+        if log["logbook_type"] in self.logger_types:
+            table = TinyDB("db.json").table("table")
+            with transaction(table) as tr:
+                tr.insert(log)
