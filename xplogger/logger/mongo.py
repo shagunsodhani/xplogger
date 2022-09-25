594554f8f73ddc1deb4ebc12e5fba63ab60b0294
Shagun Sodhani <1321193+shagunsodhani@users.noreply.github.com>
2022-09-25 11:04:30 -0400

    Check if connection to mongo db is working (#19)
    * Check if connection to mongo db is working
    
    * Add comments
    
    * Add comments
    
    * Update version

--- xplogger/logger/mongo.py
+++ xplogger/logger/mongo.py
@@ -1,9 +1,11 @@
 """Functions to interface with mongodb."""
 
 
+import warnings
 from copy import deepcopy
 
 from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 
 # Module "pymongo" does not explicitly export attribute "MongoClient"; implicit reexport disabled
 from xplogger.logger.base import Logger as BaseLogger
@@ -42,6 +44,20 @@ class Logger(BaseLogger):
             self.logger_types = set(config["logger_types"])
         self.client: MongoClient = MongoClient(config["host"], config["port"])  # type: ignore
         self.collection = self.client[config["db"]][config["collection"]]
+
+    def is_connection_working(self) -> bool:
+        """Check if the connection to the mongo server is working.
+
+        Checks the connection by issuing a dummy read query.
+        """
+        try:
+            self.collection.find_one({})
+            return True
+        except PyMongoError as error:
+            warnings.warn(
+                f"Could not connect to the MongoServer. \nError: {error}. \nDisabling Mongo Logger."
+            )
+            return False
 
     def write(self, log: LogType) -> None:
         """Write the log to mongodb.
